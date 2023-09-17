c3e49c97d15a7b6ac3a94f3b206b1dbb909c52ca
as3545 <86479682+as3545@users.noreply.github.com>
2023-09-17 14:31:25 +0530

    Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -45,23 +45,16 @@ using namespace std;
 
 
 int main() {
-    // Take a lenght
-    int lenght = 0;
-    cin >> lenght;
-    
-    // Make array
-    int* arr = new int[lenght]{0};
-    for (int i = 0; i < lenght; ++i) {
-        cin >> arr[i];
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    
-    // Printing
-    for (int i = lenght - 1; i >= 0; --i) {
-        printf("%d ", arr[i]);
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i]<<" ";
     }
-    
-    // Garbage collector
-    delete [] arr;
     
     return 0;
 }
